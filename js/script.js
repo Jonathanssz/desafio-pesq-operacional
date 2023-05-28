bd913f5416e8bfa2f7a5813586d81f0efa3366c3
Jonathan Santos <jonathanss400@hotmail.com>
2023-05-27 22:45:41 -0300

    atualizando 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function calcularSoma() {
-    var num1 = parseFloat(document.getElementById('numero1').value);
-    var num2 = parseFloat(document.getElementById('numero2').value);
-    var resultado = num1 + num2;
-    document.getElementById('resultado').textContent = 'A soma é: ' + resultado;
+    let num1 = parseFloat(document.getElementById('numero1').value);
+    let num2 = parseFloat(document.getElementById('numero2').value);
+    let resultado = num1 + num2;
+    return resultado;
+    // document.getElementById('resultado').textContent = 'A soma é: ' + resultado;
 }

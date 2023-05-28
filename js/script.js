e1e7ebee8333c4bf935742135aee73f4f3edffb8
Jonathan Santos <jonathanss400@hotmail.com>
2023-05-27 22:33:47 -0300

    Fazendo teste do script com função soma

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function calcularSoma() {
+    var num1 = parseFloat(document.getElementById('numero1').value);
+    var num2 = parseFloat(document.getElementById('numero2').value);
+    var resultado = num1 + num2;
+    document.getElementById('resultado').textContent = 'A soma é: ' + resultado;
+}
